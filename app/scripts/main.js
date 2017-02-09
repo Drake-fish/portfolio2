6d1e82ea2f56ef256eb39286e29fc4dcda72e38e
Drake Fish <Drake_fish@yahoo.com>
2017-02-09 15:16:33 -0600

    completed timeline

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,44 +21,34 @@ setTimeout(()=>{
 },500);
 });
 $(window).scroll(function(){
-  if($(this).scrollTop()>5){
+  if($(this).scrollTop()>100){
     $('.welcome').animate({opacity:1},500);
   }
-  if($(this).scrollTop()>50){
+  if($(this).scrollTop()>200){
     $('.skills').animate({opacity:1},500);
 
   }
 });
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+var items = document.querySelectorAll(".timeline li");
 
+// code for the isElementInViewport function
 
-
-
-
-
-
-var $animation_elements = $('.animation-element');
-var $window = $(window);
-
-function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
-
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
-
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
     }
-  });
+  }
 }
 
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
